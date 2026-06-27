8ea4f2f7b230a6bcce653bd6b7e7a6b6ca8da852
vrcalpha <190100403+vrcalphabet@users.noreply.github.com>
2026-06-27 23:00:48 +0900

    feat: デバッグ用で入力値を上書きする関数を追加

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,15 @@ export function parseInput<T extends string>(rawSchema: T) {
   const schemas = parseSchema(rawSchema)
   return parseLines(schemas) as InputSchema<T>
 }
+
+export function overrideInput(data: string) {
+  const normalizedData =
+    data
+      .split('\n')
+      .map((line) => line.trim())
+      .filter(Boolean)
+      .join('\n') + '\n'
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ;(globalThis as any)['$input'] = normalizedData
+}
--- src/services/readStdin.ts
+++ src/services/readStdin.ts
@@ -2,7 +2,10 @@ import fs from 'node:fs'
 import { ParserError } from './ParserError'
 
 export function readStdin() {
-  const stdin = fs.readFileSync(0, 'utf-8')
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const input = (globalThis as any)['$input'] as string | undefined
+
+  const stdin = input ?? fs.readFileSync(0, 'utf-8')
   if (!stdin) {
     throw ParserError(
       'parseInput より前に stdin を取得する処理を入れてはいけません。',
