1a6dc97cb51f9df5cd577d750a507abb5ca9ccb9
vrcalpha <190100403+vrcalphabet@users.noreply.github.com>
2026-07-02 00:49:03 +0900

    fix: ユニオン構文の間違った型推測を修正

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,34 +1,111 @@
 import { parseInput } from '../main'
 
-const input = parseInput(`
-  scalar1
-  +scalar2
-  scalar3, +scalar4, +scalar5
-  _scalar6
-  +_scalar7
-  +_scalar8, _scalar9, scalar10
-  arr1[]
-  +arr2[]
-  arr3[][]
-  +arr4[][]
-  arr5[0][]
-  +arr6[0][]
-  arr7[][N]
-  +arr8[][N]
-  arr9[0][N]
-  +arr10[0][N]
-  ...spread1
-  ...+spread2
-  ...spread3[]
-  ...+spread4[]
-  ...spread5[N]
-  ...+spread6[N]
-  bracket1[arg1, +arg2]
-  bracket2[arg3, +arg4][]
-  bracket3[arg3, +arg4][N]
-  bracket4[_arg1, arg2, +_arg3]
-  bracket5[_arg1, arg2, +_arg3][]
-  
-  # comment1
+const $scalar = parseInput(`
+  scalar1_1
+  +scalar1_2
+  +~scalar1_3
+  scalar1_4={N|E|S|W}
+  scalar2_1, +scalar2_2, +~scalar2_3, scalar2_4={N|E|S|W}
 `)
-console.log(input)
+
+const $spread = parseInput(`
+  ...spread1_1
+  ...+spread1_2
+  ...+~spread1_3
+  ...spread1_4={N|E|S|W}
+  ...spread2_1[]
+  ...+spread2_2[]
+  ...+~spread2_3[]
+  ...spread2_4={N|E|S|W}[]
+  ...spread3_1[N]
+  ...+spread3_2[N]
+  ...+~spread3_3[N]
+  ...spread3_4={N|E|S|W}[N]
+`)
+
+const $arr = parseInput(`
+  arr1_1[]
+  +arr1_2[]
+  +~arr1_3[]
+  arr1_4={N|E|S|W}[]
+  arr2_1[][]
+  +arr2_2[][]
+  +~arr2_3[][]
+  arr2_4={N|E|S|W}[][]
+  arr3_1[0][]
+  +arr3_2[0][]
+  +~arr3_3[0][]
+  arr3_4={N|E|S|W}[0][]
+  arr4_1[][N]
+  +arr4_2[][N]
+  +~arr4_3[][N]
+  arr4_4={N|E|S|W}[][N]
+  arr5_1[0][N]
+  +arr5_2[0][N]
+  +~arr5_3[0][N]
+  arr5_4={N|E|S|W}[0][N]
+`)
+
+const $bracket = parseInput(`
+  bracket1_1[arg1_1, +arg1_2, +~arg1_3, arg1_4={N|E|S|W}]
+  bracket1_2[_arg1_1, +_arg1_2, +~_arg1_3, _arg1_4={N|E|S|W}]
+  bracket2_1[arg1_1, +arg1_2, +~arg1_3, arg1_4={N|E|S|W}][]
+  bracket2_2[_arg1_1, +_arg1_2, +~_arg1_3, _arg1_4={N|E|S|W}][]
+  bracket3_1[arg1_1, +arg1_2, +~arg1_3, arg1_4={N|E|S|W}][N]
+  bracket3_2[_arg1_1, +_arg1_2, +~_arg1_3, _arg1_4={N|E|S|W}][N]
+`)
+
+const $_scalar = parseInput(`
+  _scalar1_1
+  +_scalar1_2
+  +~_scalar1_3
+  _scalar1_4={N|E|S|W}
+  _scalar2_1, +_scalar2_2, +~_scalar2_3, _scalar2_4={N|E|S|W}
+`)
+
+const $_spread = parseInput(`
+  ..._spread1_1
+  ...+_spread1_2
+  ...+~_spread1_3
+  ..._spread1_4={N|E|S|W}
+  ..._spread2_1[]
+  ...+_spread2_2[]
+  ...+~_spread2_3[]
+  ..._spread2_4={N|E|S|W}[]
+  ..._spread3_1[N]
+  ...+_spread3_2[N]
+  ...+~_spread3_3[N]
+  ..._spread3_4={N|E|S|W}[N]
+`)
+
+const $_arr = parseInput(`
+  _arr1_1[]
+  +_arr1_2[]
+  +~_arr1_3[]
+  _arr1_4={N|E|S|W}[]
+  _arr2_1[][]
+  +_arr2_2[][]
+  +~_arr2_3[][]
+  _arr2_4={N|E|S|W}[][]
+  _arr3_1[0][]
+  +_arr3_2[0][]
+  +~_arr3_3[0][]
+  _arr3_4={N|E|S|W}[0][]
+  _arr4_1[][N]
+  +_arr4_2[][N]
+  +~_arr4_3[][N]
+  _arr4_4={N|E|S|W}[][N]
+  _arr5_1[0][N]
+  +_arr5_2[0][N]
+  +~_arr5_3[0][N]
+  _arr5_4={N|E|S|W}[0][N]
+`)
+
+const $_bracket = parseInput(`
+  _bracket1_1[arg1_1, +arg1_2, +~arg1_3, arg1_4={N|E|S|W}]
+  _bracket1_2[_arg1_1, +_arg1_2, +~_arg1_3, _arg1_4={N|E|S|W}]
+  _bracket2_1[arg1_1, +arg1_2, +~arg1_3, arg1_4={N|E|S|W}][]
+  _bracket2_2[_arg1_1, +_arg1_2, +~_arg1_3, _arg1_4={N|E|S|W}][]
+  _bracket3_1[arg1_1, +arg1_2, +~arg1_3, arg1_4={N|E|S|W}][N]
+  _bracket3_2[_arg1_1, +_arg1_2, +~_arg1_3, _arg1_4={N|E|S|W}][N]
+`)
--- src/types/inputSchema.ts
+++ src/types/inputSchema.ts
@@ -2,19 +2,36 @@
 
 // #region Utils
 
+/** 先頭と末尾の空白文字を削除する。 */
 type Trim<T extends string> =
   T extends ` ${infer R}` ? Trim<R>
   : T extends `${infer L} ` ? Trim<L>
   : T
 
-type RemoveComment<T extends string> =
-  T extends `${infer L}#${string}` ? Trim<L> : Trim<T>
+/** コメントを除去したうえで、トリムしたものを返す。 */
+type RemoveComment<T extends string> = Trim<T extends `${infer L}#${string}` ? L : T>
 
+/** キーと値から、連想配列を作成する。 */
 type O<Name extends string, T> = { [K in Name]: T }
 
+/** ホバー時に見やすいように複雑な型を展開する。 */
 type Prettify<T> = { [K in keyof T]: T[K] } & {}
 
+/** 先頭文字がアンダーバー（無視する変数）かどうかを判定して返す。 */
 type CheckIgnoreVar<Name extends string, T> = Name extends `_${string}` ? {} : T
+
+/** 文字列を改行文字で分割して返す。 */
+type SplitLines<T extends string> =
+  T extends `${infer L}\n${infer R}` ? [RemoveComment<L>, ...SplitLines<R>]
+  : [RemoveComment<T>]
+
+type Dimension<T, K> =
+  K extends void ? T
+  : K extends [] ? T[]
+  : K extends (infer U)[] ? Dimension<T, U>[]
+  : never
+
+type Flatten<T> = T extends (infer U)[] ? U : never
 
 // #endregion
 // #region Errors
@@ -48,6 +65,7 @@ type NumericChar =
 
 type IdentBodyChar = AlphaChar | NumericChar
 
+/** 文字列が、英数字または記号（`$`, `_`）の繰り返しであるかどうかを判定する。 */
 type IsIdentBody<T extends string> =
   T extends `${infer Head}${infer Tail}` ?
     Head extends IdentBodyChar ?
@@ -57,7 +75,9 @@ type IsIdentBody<T extends string> =
     : false
   : true
 
+/** 有効な識別子であるかどうかを判定し、Thenを返す。無効である場合は、エラーを返す。 */
 type IfIdent<T extends string, Raw extends string, Then> =
+  // "scalar1 scalar2" などは、カンマを忘れているのではないか？
   T extends `${string} ${string}` ? O<Raw, Err_InvalidSyntaxForgetComma>
   : T extends `${infer Head}${infer Tail}` ?
     Head extends AlphaChar ?
@@ -71,12 +91,14 @@ type IfIdent<T extends string, Raw extends string, Then> =
 // #endregion
 // #region Schema
 
+/** 連想配列内に、"\_\_ERROR__"というキーが含まれているかを再帰的に探索する。 */
 type HasError<T> =
   T extends unknown[] ? HasError<T[number]>
   : T extends object ?
     { [K in keyof T]: K extends '__ERROR__' ? true : HasError<T[K]> }[keyof T]
   : false
 
+/** 型にエラーが含まれている場合は、エラーオブジェクトに変換する。 */
 type CheckHasError<T> =
   HasError<T> extends false ? T
   : {
@@ -86,88 +108,73 @@ type CheckHasError<T> =
       }
     }
 
-type SplitLines<T extends string> =
-  T extends `${infer L}\n${infer R}` ? [L, ...SplitLines<R>] : [T]
-
-type ParseLines<T extends string[]> =
-  T extends [infer L extends string, ...infer Rest extends string[]] ?
-    (RemoveComment<L> extends '' ? {} : ParseLine<RemoveComment<L>>) &
-      ParseLines<Rest>
-  : {}
-
-type ParseArrayFieldSchema<
-  Name extends string,
-  Index extends number | '',
-  TwoD extends boolean,
-> =
-  Index extends '' ? ParseField<Name, TwoD, number[], string[]>
-  : ParseField<Name, TwoD, number, string>
-
-type ParseSpreadField<Name extends string, TwoD extends boolean> = ParseField<
-  Name,
-  TwoD,
-  number[],
-  string[]
->
+type ParseArrayFieldSchema<Name extends string, Index extends number | '', D> =
+  Index extends '' ? ParseField<Name, D> : ParseField<Name, Flatten<D>>
 
 type ParseBracketArrayField<
   Name extends string,
   Args extends string,
-  TwoD extends boolean,
-> = O<Name, TwoDimension<Prettify<ParseScalar<Args>>, TwoD>>
+  D = void,
+> = CheckIgnoreVar<Name, O<Name, Dimension<Prettify<ParseScalar<Args>>, D>>>
 
-type TwoDimension<T, TwoD extends boolean> = TwoD extends false ? T : T[]
+// type TwoDimension<T, TwoD extends boolean> = TwoD extends false ? T : T[]
 
-type ParseField<
-  T extends string,
-  TwoD extends boolean = false,
-  Num = number,
-  Str = string,
-> =
+type ParseField<T extends string, D = void> =
+  // 行の末尾にカンマがあるか、カンマの後に配列表記が続いている
   T extends `${string},` ? O<T, Err_InvalidSyntaxWithExtraSymbol>
   : T extends '' ? O<',', Err_InvalidSyntaxWithExtraSymbol>
   : T extends `+~${infer Name}` ?
-    CheckIgnoreVar<Name, IfIdent<Name, T, O<Name, TwoDimension<Num, TwoD>>>>
+    CheckIgnoreVar<Name, IfIdent<Name, T, O<Name, Dimension<number, D>>>>
   : T extends `+${infer Name}` ?
-    CheckIgnoreVar<Name, IfIdent<Name, T, O<Name, TwoDimension<Num, TwoD>>>>
-  : ParseIdent<T, Str, TwoD>
+    CheckIgnoreVar<Name, IfIdent<Name, T, O<Name, Dimension<number, D>>>>
+  : ParseIdent<T, D>
 
-type ParseIdent<T extends string, V, TwoD extends boolean> =
+type ParseIdent<T extends string, D> =
   T extends `${infer Name}={${infer Union}}` ?
-    CheckIgnoreVar<
-      Name,
-      IfIdent<Name, T, O<Name, TwoDimension<keyof ParseUnion<Union>, TwoD>>>
-    >
-  : CheckIgnoreVar<T, IfIdent<T, T, O<T, TwoDimension<V, TwoD>>>>
+    CheckIgnoreVar<Name, IfIdent<Name, T, O<Name, Dimension<ParseUnion<Union>, D>>>>
+  : CheckIgnoreVar<T, IfIdent<T, T, O<T, Dimension<string, D>>>>
 
 type ParseScalar<T extends string> =
   T extends `${infer L},${infer Rest}` ? ParseField<Trim<L>> & ParseScalar<Rest>
   : ParseField<Trim<T>>
 
+/** ユニオン文字列（例：`N|E|S|W`）をユニオン型（例：`"N" | "E" | "S" | "W"`）に変換する。 */
 type ParseUnion<T extends string> =
-  T extends `${infer L}|${infer Rest}` ? O<L, never> & ParseUnion<Rest> : O<T, never>
+  T extends `${infer L}|${infer Rest}` ? L | ParseUnion<Rest> : T
 
-type CheckLineCount<LineCount extends string, Raw extends string, T> =
-  LineCount extends '' ? T
-  : LineCount extends `${number}` ? T
-  : IfIdent<LineCount, Raw, T>
+type ValidateLineCount<LineCount extends string, Raw extends string, T> =
+  LineCount extends '' ? T : IfIdent<LineCount, Raw, T>
 
+/** 改行文字で分割した各行を変換する。 */
 type ParseLine<T extends string> =
+  // `...spread[]` または `...spread[N]`
   T extends `...${infer Name}[${infer LineCount}]` ?
-    CheckLineCount<LineCount, T, ParseSpreadField<Name, true>>
-  : T extends `...${infer Name}` ? ParseSpreadField<Name, false>
-  : T extends (
-    `${infer Name}[${infer Index extends number | ''}][${infer LineCount}]`
-  ) ?
-    CheckLineCount<LineCount, T, ParseArrayFieldSchema<Name, Index, true>>
-  : T extends `${infer Name}[${infer Args}][${infer LineCount}]` ?
-    CheckLineCount<LineCount, T, ParseBracketArrayField<Name, Args, true>>
-  : T extends `${infer Name}[${infer Args}]` ?
+    ValidateLineCount<LineCount, T, ParseField<Name, [][]>>
+  : // `...spread`
+  T extends `...${infer Name}` ? ParseField<Name, []>
+  : // `arr[][]` または `arr[0][]` または `arr[][N]` または `arr[0][N]`
+  T extends `${infer Name}[${infer Index extends number | ''}][${infer LineCount}]` ?
+    ValidateLineCount<LineCount, T, ParseArrayFieldSchema<Name, Index, [][]>>
+  : // `bracket[arg][]` または `bracket[arg][N]`
+  T extends `${infer Name}[${infer Args}][${infer LineCount}]` ?
+    ValidateLineCount<LineCount, T, ParseBracketArrayField<Name, Args, []>>
+  : // `arr[]` または `bracket[arg]`
+  T extends `${infer Name}[${infer Args}]` ?
     Args extends '' ?
-      ParseArrayFieldSchema<Name, '', false>
-    : ParseBracketArrayField<Name, Args, false>
-  : ParseScalar<T>
+      // `arr[]`
+      ParseArrayFieldSchema<Name, '', []>
+    : // `bracket[arg]`
+      ParseBracketArrayField<Name, Args>
+  : // 上記のすべてに当てはまらなかったら、スカラ値（`scalar`, `scalar1, scalar2, ...`）
+    ParseScalar<T>
 
+/** 各行を変換する。 */
+type ParseLines<T extends string[]> =
+  T extends [infer L extends string, ...infer Rest extends string[]] ?
+    (L extends '' ? {} : ParseLine<L>) & ParseLines<Rest>
+  : {}
+
+/** 型のエントリポイント。文字列の分割、変換、エラーチェック、整形を行う。 */
 export type InputSchema<T extends string> =
   string extends T ? never : Prettify<CheckHasError<ParseLines<SplitLines<T>>>>
 
